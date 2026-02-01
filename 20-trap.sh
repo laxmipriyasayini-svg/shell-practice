5ec501a646275749f66e12a99d25c0edac69c56e
laxmipriya <laxmipriyasayini@gmail.com>
2026-02-01 20:26:00 +0530

    actions

--- 20-trap.sh
+++ 20-trap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-trap 'echo"There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
