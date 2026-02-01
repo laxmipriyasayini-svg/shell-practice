091e2e1a9995d0adb384cd3bd05493d86e792c9b
laxmipriya <laxmipriyasayini@gmail.com>
2026-02-01 12:21:41 +0530

    actions

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hello Ramesh  How are you?"
+echo "Ramesh:: Hi Suresh,  am fine thanks for asking"
+echo "Suresh:: doing fine. what are you learning Ramesh?"
+echo "Ramesh:: I am learning shell. What about you?"
