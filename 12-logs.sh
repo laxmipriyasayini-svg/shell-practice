278124a08e3bbfc7cd45e0bc19391386312887e9
laxmipriya <laxmipriyasayini@gmail.com>
2026-02-01 17:29:34 +0530

    actions

--- 12-logs.sh
+++ 12-logs.sh
@@ -8,10 +8,12 @@ echo "Please run this script with root user access"
 exit 1
 fi
 
+mkdir -p $LOGS_FOLDER
+
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-    echo "$2 ...FAILURE" 
+    echo "$2 ...FAILURE" | tee -a $LOGS_FILE
 exit 1
 else
 echo "$2 ...SUCCESS" | tee -a $LOGS_FILE
