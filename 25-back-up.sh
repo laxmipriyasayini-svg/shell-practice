ae0d16dabeb02dc3eccc58e909e6238297d9d77a
laxmipriya <laxmipriyasayini@gmail.com>
2026-02-07 13:59:48 +0530

    actions

--- 25-back-up.sh
+++ 25-back-up.sh
@@ -51,4 +51,7 @@ log "Source Directory: $SOURCE_DIR"
 log "Destination Directory: $DEST_DIR"
 log "Days: $DAYS"
  
+ if [ -z $FILES ]; then
+  log -e "No files to archieve ... $Y Skipping $N"
+  fi
 
