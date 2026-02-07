1f73b8363ab49996e7d38d520d299a80dfe1da0f
laxmipriya <laxmipriyasayini@gmail.com>
2026-02-07 13:51:09 +0530

    actions

--- 25-back-up.sh
+++ 25-back-up.sh
@@ -11,6 +11,7 @@ N="\e[0m"
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} # 14 days i the default valuve, if the user not supplied
+
 if [ $USERID -ne 0 ]; then
    echo -e " $R Please run this script with root user access $N"
    exit 1
@@ -20,18 +21,34 @@ if [ $USERID -ne 0 ]; then
   
   USAGE(){
     echo -e " $R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> [default 14 days] $N"
+    exit 1
+  }
+
+  log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
   }
 
   if [ $# -lt 2 ]; then
     USAGE
     fi
 
+
 if [ ! -d $SOURCE_DIR ]; then
-    echo -e "$R $SOURCE_DIR does not exist $N"
+    echo -e "$R source directory: $SOURCE_DIR does not exist $N"
     exit 1
 fi
 
 if [ ! -d $DEST_DIR ]; then
-    echo -e "$R $DEST_DIR does not exist $N"
+    echo -e "$R Destination directory:$DEST_DIR does not exist $N"
     exit 1
 fi
+
+### Find the files
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+log "Backup started"
+log "Source Directory: $SOURCE_DIR"
+log "Destination Directory: $DEST_DIR"
+log "Days: $DAYS"
+ 
+
