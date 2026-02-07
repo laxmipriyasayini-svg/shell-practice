63a9d88539e46a34dfb2de1c9ae2cd579afcbbc9
laxmipriya <laxmipriyasayini@gmail.com>
2026-02-07 23:42:14 +0530

    actions

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -19,7 +19,7 @@ log(){
      PARTITION=$(echo $line | awk '{print $7}')
 
   if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-     MESSAGE+="High Disk usage on $PARTITION:$USAGE% \n"
+     MESSAGE+="High Disk usage on $PARTITION: $USAGE% <br>"
   fi
 
   # done <<< $DISK_USAGE
